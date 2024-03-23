a384c472050c3ce58e08625eb28682aa11f503e7
Bruno Pergher <brunopergher_1@hotmail.com>
2024-03-23 17:19:37 -0300

    Started Implement flask security for login/register and control access

--- E-Stagio/App/__init__.py
+++ E-Stagio/App/__init__.py
@@ -2,10 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
+from flask_security import Security, SQLAlchemyUserDatastore
 
+# Inicialize o Flask
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-from App import routes, models
+# Inicialize o SQLAlchemy
+db = SQLAlchemy(app)
+
+# Agora é seguro importar os modelos porque 'db' já foi definido
+from App.models import User, Role
+
+migrate = Migrate(app, db)
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+# Importe as rotas após a inicialização dos modelos e extensões
+from App import routes
--- E-Stagio/App/models.py
+++ E-Stagio/App/models.py
@@ -1,19 +1,21 @@
+import uuid
 from App import db
+from flask_security import UserMixin, RoleMixin
 
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    users = db.relationship('User', backref='role', lazy=True)
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'<Role {self.name}>'
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
-    password_hash = db.Column(db.Text)
+    roles = db.relationship('Role', backref=db.backref('users', lazy='dynamic'))
 
     def __repr__(self):
         return f'<User {self.username}>'
