034510a2cae097515b8e072cdba038815708e993
Bruno Pergher <brunopergher_1@hotmail.com>
2024-11-04 00:37:28 -0300

    Update e1eae03219ac_add_roles_and_admin_user.py

--- E-Stagio Prod/migrations/versions/e1eae03219ac_add_roles_and_admin_user.py
+++ E-Stagio Prod/migrations/versions/e1eae03219ac_add_roles_and_admin_user.py
@@ -1,31 +1,35 @@
-from datetime import datetime
+"""Add roles and admin user
+
+Revision ID: e1eae03219ac
+Revises: c594222efc6c
+Create Date: 2024-11-03 23:38:58.238535
+
+"""
+from alembic import op
+from flask import current_app
 from flask_security.utils import hash_password
 
-def upgrade():
-    # Define a senha hash para o usuário admin
-    hashed_password = hash_password('Admin123!')
-    current_date = datetime.utcnow().isoformat()  # Data atual em UTC
+# revision identifiers, used by Alembic.
+revision = 'e1eae03219ac'
+down_revision = 'c594222efc6c'
+branch_labels = None
+depends_on = None
 
-    # Insere os papéis (roles) diretamente
+def upgrade():
     op.execute("INSERT INTO role (name, description) VALUES ('professor', 'Professor Role')")
     op.execute("INSERT INTO role (name, description) VALUES ('empresa', 'Empresa Role')")
     op.execute("INSERT INTO role (name, description) VALUES ('supervisor', 'Supervisor Role')")
     op.execute("INSERT INTO role (name, description) VALUES ('aluno', 'Aluno Role')")
     op.execute("INSERT INTO role (name, description) VALUES ('admin', 'Admin Role')")
-
-    # Insere o usuário admin com confirmed_at definido para hoje
+    
+    # Inserção do usuário admin diretamente
+    hashed_password = hash_password('Admin123!')
+    confirmed_date = '2024-11-04 00:00:00'
     op.execute(f"""
         INSERT INTO "user" (username, email, password, active, fs_uniquifier, confirmed_at)
-        VALUES (
-            'admin',
-            'admin@admin.com',
-            '{hashed_password}',
-            TRUE,
-            'unique_identifier_for_admin',
-            '{current_date}'
-        )
+        VALUES ('admin', 'admin@admin.com', '{hashed_password}', TRUE, 'unique_identifier_for_admin', '{confirmed_date}')
     """)
-
+    
     # Associando o papel de admin ao usuário admin criado
     op.execute("""
         INSERT INTO roles_users (user_id, role_id)
