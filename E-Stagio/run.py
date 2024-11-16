d52128bf048e8af5ce09271a2a09fe554512c61a
Bruno Pergher <brunopergher_1@hotmail.com>
2024-11-16 18:00:30 -0300

    removed old code

--- E-Stagio/run.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from app.models import User, Role
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Role': Role}
-
-if __name__ == '__main__':
-    app.run(debug=True)
